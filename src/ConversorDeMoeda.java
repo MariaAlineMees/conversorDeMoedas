911771ae2a50a035318a04ac2e6dcf0183e51834
MariaAlineMees <mariaalinemees@gmail.com>
2025-05-05 16:20:02 -0300

    git rm --cached .env

--- src/ConversorDeMoeda.java
+++ src/ConversorDeMoeda.java
@@ -1,8 +1,7 @@
-
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
-
+import io.github.cdimascio.dotenv.Dotenv;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -14,41 +13,33 @@ public class ConversorDeMoeda {
 
         HttpClient cliente = HttpClient.newHttpClient();
 
-        final String apiKey = System.getenv("EXCHANGE_API_KEY");
+        // Carrega variáveis do arquivo .env
+        Dotenv dotenv = Dotenv.load();
+        final String apiKey = dotenv.get("EXCHANGE_API_KEY");
 
         if (apiKey == null) {
-            System.err.println("Erro: variável de ambiente EXCHANGE_API_KEY não encontrada.");
+            System.err.println("Erro: variável EXCHANGE_API_KEY não encontrada no arquivo .env");
             return 0;
         }
 
         String url = String.format("https://v6.exchangerate-api.com/v6/%s/pair/%s/%s", apiKey, origem, destino);
         System.out.println("URL segura: " + url);
 
-
-        //Chave API retirada por seguranca!
-
-
         HttpRequest requisicao = HttpRequest.newBuilder()
                 .uri(URI.create(url))
                 .build();
 
-
         HttpResponse<String> resposta = cliente.send(requisicao, HttpResponse.BodyHandlers.ofString());
 
-
-
         JsonObject json = JsonParser.parseString(resposta.body()).getAsJsonObject();
-
 
         if (json.has("conversion_rate")) {
             double taxa = json.get("conversion_rate").getAsDouble();
             double resultado = valor * taxa;
 
-
             System.out.printf("\n%.2f %s (%s) = %.2f %s (%s)\n",
                     valor, origem, origem,
                     resultado, destino, destino);
-
 
             System.out.printf("\nAtualmente, 1 %s equivale a %.4f %s.\n", origem, taxa, destino);
 
